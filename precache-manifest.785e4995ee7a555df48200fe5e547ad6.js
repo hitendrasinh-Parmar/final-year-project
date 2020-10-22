bf2cc7b60f30f91af1ba0ba3bc486c64a7756559
hitendrasinh-Parmar <hitrajput098@gmail.com>
2020-10-22 11:52:24 +0530

    Updates

---
+++ precache-manifest.785e4995ee7a555df48200fe5e547ad6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad5f18ac4fe3dd32904300e78c4ba7f2",
+    "url": "/final-year-project/index.html"
+  },
+  {
+    "revision": "aeba05fed5a85a263d77",
+    "url": "/final-year-project/static/css/main.10e52fd5.chunk.css"
+  },
+  {
+    "revision": "2562af8d7cfeb4f736ac",
+    "url": "/final-year-project/static/js/2.13a6792c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/final-year-project/static/js/2.13a6792c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeba05fed5a85a263d77",
+    "url": "/final-year-project/static/js/main.698a9218.chunk.js"
+  },
+  {
+    "revision": "5bba6e19d35c61e1e99e",
+    "url": "/final-year-project/static/js/runtime-main.d0ba2a48.js"
+  }
+]);
